2a7cdde1c61a03ce12e5ecb76e968a943660c2d7
sehnxxxx <13736471390@163.com>
2025-09-08 20:54:33 +0800

    2

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
-
+import Link from "next/link"
 
 export default function HomePage() {
   return(
       <main className="p-6">
           <h1 className="text-3xl font-bold">璑闇矢鳕</h1>
           <p>wuanshixue</p>
+
+          <Link href={"/blog"}>
+              <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+                  blog
+              </button>
+          </Link>
       </main>
   )
 }
